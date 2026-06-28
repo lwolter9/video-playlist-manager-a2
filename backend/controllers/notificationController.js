1ac1c5447b7cff97ecaa18fa689c55f52028ecfa
Leroy Wolter <leroy.wolter1@gmail.com>
2026-06-28 14:38:08 +1000

    Fix corrupted notification implementation files

--- backend/controllers/notificationController.js
+++ backend/controllers/notificationController.js
@@ -1,6 +1,3 @@
-86% of storage used … If you run out, you can't create, edit, and upload files. Get 30 GB for ₹15 for 3 months ₹59.
-1
-100%
 const Notification = require('../models/Notification');
 
 const getNotifications = async (req, res) => {
